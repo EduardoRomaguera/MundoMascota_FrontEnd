455320c6fca65e3b4b7a4ab11ff955c46f464a3b
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-03-13 16:41:33 -0600

    Revert "Avances index.html"
    This reverts commit e3dbe5fcc0be9672ecf552c98c0e511bec63a0a5.

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 
-// Control de SlideB Atras y Adelante
-var slideIndexB = 1;
-showSlidesB(slideIndexB);
-
-function plusSlidesB(n) {
-    showSlidesB(slideIndexB += n);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showSlidesB(n) {
-    var ii;
-    var slidesB = document.getElementsByClassName("mySlidesB");
-    if (n > slidesB.length) {slideIndexB = 1}
-    if (n < 1) {slideIndexB = slidesB.length}
-    for (ii = 0; ii < slidesB.length; ii++) {
-    slidesB[ii].style.display = "none";
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-    slidesB[slideIndexB-1].style.display = "block";
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
