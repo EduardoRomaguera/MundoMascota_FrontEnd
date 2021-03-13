e3dbe5fcc0be9672ecf552c98c0e511bec63a0a5
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-03-13 16:37:59 -0600

    Avances index.html
    Creación de segundo slide para sección de Patrocinadores.

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,19 @@
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+// Control de SlideB Atras y Adelante
+var slideIndexB = 1;
+showSlidesB(slideIndexB);
+
+function plusSlidesB(n) {
+    showSlidesB(slideIndexB += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function showSlidesB(n) {
+    var ii;
+    var slidesB = document.getElementsByClassName("mySlidesB");
+    if (n > slidesB.length) {slideIndexB = 1}
+    if (n < 1) {slideIndexB = slidesB.length}
+    for (ii = 0; ii < slidesB.length; ii++) {
+    slidesB[ii].style.display = "none";
 }
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    slidesB[slideIndexB-1].style.display = "block";
 }
