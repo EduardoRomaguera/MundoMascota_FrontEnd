78b5b3281a5be7ca48f92fdf1214e1a1b96be424
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-03-16 15:20:40 -0600

    Validaciones y login
    Página de contacto lista al 100%
    Validación de correo en inicio de sesión lista.
    Validación de datos de inicio de sesión lista.
    Inicio de sesión listo.
    Redirección de inicio de sesión listo.
    Falta redirección de seguridad, está buggy

--- scripts/servicios.js
+++ scripts/servicios.js
@@ -105,3 +105,93 @@ let listaVacunas = [{
     'especie': 'Gato',
     'estado': 'Áctivo'
 }]
+
+// Lista de usuarios para probar inicio de sesión
+let listaUsuarios = [{
+    'correo': 'admin@admin.com',
+    'nombre': 'Raúl Pérez',
+    'nacimiento': '09-20-1973',
+    'sexo': 'Masculino',
+    'contrasenna': '123',
+    'tipo': 'Administrador'
+}, {
+    'correo': 'cliente@cliente.com',
+    'nombre': 'Amanda Víquez',
+    'nacimiento': '03-11-1991',
+    'sexo': 'Femenino',
+    'contrasenna': '123',
+    'tipo': 'Cliente'
+},
+{
+    'correo': 'proveedor@proveedor.com',
+    'nombre': 'Pablo Monestel',
+    'nacimiento': '10-12-1987',
+    'sexo': 'Masculino',
+    'contrasenna': '123',
+    'tipo': 'Proveedor'
+},
+{
+    'correo': 'jose@gmail.com',
+    'nombre': 'José Ramírez',
+    'nacimiento': '03-11-1990',
+    'sexo': 'Masculino',
+    'contrasenna': '123Abc!',
+    'tipo': 'Cliente'
+}
+];
+
+//Codigo para validar la información de usuario y contraseña
+
+const iniciarSesion = (correo, contrasenna) => {
+    let credencialesCorrectas = false;
+    listaUsuarios.forEach(usuario => {
+        if (usuario.correo == correo) {
+            if (usuario.contrasenna == contrasenna) {
+                credencialesCorrectas = true;
+                sessionStorage.setItem('usuarioConectado', JSON.stringify(usuario));
+            }
+        }
+    });
+
+    if (credencialesCorrectas == true) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+        Toast.fire({
+            icon: 'success',
+            title: 'Iniciando sesión'
+        })
+        //Codigo para redireccionar según el tipo de usuario correspondiente
+        .then(() => {
+            let usuario;
+            sessionStorage.getItem('usuarioConectado')
+            usuario = JSON.parse(sessionStorage.getItem('usuarioConectado'));
+                switch (usuario.tipo) {
+                    case 'Administrador':
+                        window.location.href = 'P29-principal-administrador.html';
+                        break;
+                    case 'Cliente':
+                        window.location.href = 'P09-principal-cliente.html';
+                        break;
+                    case 'Proveedor':
+                        window.location.href = 'P19-principal-proveedor.html';
+                        break;
+                }
+        });
+    } else {
+        Swal.fire({
+            imageUrl: "images/error2.jpg",
+            title: "¡OOPS! Algo pasó aqui",
+            text: "Usuario o contraseña incorrectos",
+            confirmButtonText: "Intentar de nuevo"
+        });
+    }
+};
