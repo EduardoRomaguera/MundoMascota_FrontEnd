a54fc5b3bda1d7128d473dc5b773eef1f0959bdb
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-03-23 03:37:20 -0600

    Landingpage Sochi 2.0
    En proceso, 50% avance

---
+++ scripts/P00-landing-sochi2.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {mostrar(), sochi()};
+
+function mostrar() {
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 510) {
+    document.getElementById("nav").style.top = "0";
+} else {
+    document.getElementById("nav").style.top = "-60px";
+}
+}
+
+function sochi() {
+if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 180) {
+    document.getElementById("romano").style.display = "inline";
+} else {
+    document.getElementById("romano").style.display = "none";
+}
+}
