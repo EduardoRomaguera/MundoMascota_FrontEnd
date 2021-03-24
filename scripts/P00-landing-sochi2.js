24572f42bb8d66c97ce88f5b1d277e2baf4420e3
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-03-24 01:47:37 -0600

    Cambios varios
    Cambio tamaño imagen miembros Sochi
    Video en fondo de palabra SOMOS.
    Inicio mejora a P54-Tienda

--- scripts/P00-landing-sochi2.js
+++ scripts/P00-landing-sochi2.js
@@ -12,8 +12,12 @@ function sochi() {
 if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 180) {
     document.getElementById("romano").style.display = "inline";
     document.getElementById("somo").className = "blanco";
+    document.getElementById("overlay").className = "blanco";
+    document.getElementById("video").style.vid.autoplay = true;
 } else {
     document.getElementById("romano").style.display = "none";
     document.getElementById("somo").className =  "negro";
+    document.getElementById("overlay").className = "negro";
+    document.getElementById("video").style.vid.autoplay = true;
 }
 }
