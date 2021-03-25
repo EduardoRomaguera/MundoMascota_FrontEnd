7c5dd7139313a9378069eca0156c0da2e47657d3
Kathy <ksegurag1@ucenfotec.ac.cr>
2021-03-25 01:11:37 -0600

    arreglo de pagina p99 con filtro

--- scripts/P99-reporte-baneados.js
+++ scripts/P99-reporte-baneados.js
@@ -1,17 +1,25 @@
 'use strict';
 
 const TABLAUSUARIOSBAN = document.querySelector('#tbl-usurios-ban tbody');
-const MOSTRARTABLAUSUARIOSBAN = () => {
-    listaUsuariosBaneados.forEach(usuario => {
-        let fila = TABLAUSUARIOSBAN.insertRow();
-        fila.insertCell().innerHTML = usuario.nombre;
-        fila.insertCell().innerHTML = usuario.correo;
-        fila.insertCell().innerHTML = usuario.sexo;
-        fila.insertCell().innerHTML = usuario.tipo;
-        fila.insertCell().innerHTML = usuario.estado;
-        fila.insertCell().innerHTML = usuario.Razon;
+const FILTROUSUARIOS = document.querySelector('#txt-filtro-usuarios-baneados');
 
+
+const MOSTRARTABLAUSUARIOSBAN = () => {
+    let filtro = FILTROUSUARIOS.value.toLowerCase();
+    TABLAUSUARIOSBAN.innerHTML = '';
+    listaUsuariosBaneados.forEach(usuario => {
+        if (usuario.nombre.toLowerCase().includes(filtro)) {
+
+            let fila = TABLAUSUARIOSBAN.insertRow();
+
+            fila.insertCell().innerHTML = usuario.nombre;
+            fila.insertCell().innerHTML = usuario.correo;
+            fila.insertCell().innerHTML = usuario.sexo;
+            fila.insertCell().innerHTML = usuario.tipo;
+            fila.insertCell().innerHTML = usuario.estado;
+            fila.insertCell().innerHTML = usuario.Razon;
+        }
     });
 };
-
 MOSTRARTABLAUSUARIOSBAN();
+FILTROUSUARIOS.addEventListener('keyup', MOSTRARTABLAUSUARIOSBAN);
