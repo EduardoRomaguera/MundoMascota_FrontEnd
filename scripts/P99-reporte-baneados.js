968d0713da2b2d774dfdd1883493e3df56ea8f4d
Kathy <ksegurag1@ucenfotec.ac.cr>
2021-03-25 01:16:54 -0600

    arreglo de reporte de usuarios baneados

--- scripts/P99-reporte-baneados.js
+++ scripts/P99-reporte-baneados.js
@@ -14,9 +14,7 @@ const MOSTRARTABLAUSUARIOSBAN = () => {
 
             fila.insertCell().innerHTML = usuario.nombre;
             fila.insertCell().innerHTML = usuario.correo;
-            fila.insertCell().innerHTML = usuario.sexo;
             fila.insertCell().innerHTML = usuario.tipo;
-            fila.insertCell().innerHTML = usuario.estado;
             fila.insertCell().innerHTML = usuario.Razon;
         }
     });
