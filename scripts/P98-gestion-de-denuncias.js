6ffaa9b92a3e5544ee4811865af47ccf3dbad5c7
alejandro290119 <80568744+alejandro290119@users.noreply.github.com>
2021-04-30 19:24:19 -0500

    cambios ortograficos
    cambios ortograficos

--- scripts/P98-gestion-de-denuncias.js
+++ scripts/P98-gestion-de-denuncias.js
@@ -10,59 +10,60 @@ const MOSTRARTABLADENUNCIAS = () => {
     listaUsuariosReportados.forEach(reporte => {
         if (reporte.Usuario.toLowerCase().includes(filtro)) {
 
-        let fila = TABLADENUNCIAS.insertRow();
-        fila.insertCell().innerHTML = reporte.Usuario;
-        fila.insertCell().innerHTML = reporte.Razon;
-        fila.insertCell().innerHTML = reporte.Descripcion;
+            let fila = TABLADENUNCIAS.insertRow();
+            fila.insertCell().innerHTML = reporte.Usuario;
+            fila.insertCell().innerHTML = reporte.Razon;
+            fila.insertCell().innerHTML = reporte.Descripcion;
 
-        let celdaAcciones = fila.insertCell();
+            let celdaAcciones = fila.insertCell();
 
-        let botonBanear = document.createElement('button');
-        botonBanear.innerText = 'Banear';
+            let botonBanear = document.createElement('button');
+            botonBanear.innerText = 'Bloquear';
 
-        botonBanear.addEventListener('click', () => {
-            Swal.fire({
-                'icon': 'warning',
-                'text': '¿Está seguro que desea banear al usuario?',
-                'showCancelButton': true,
-                'confirmButtonText': 'Sí, estoy seguro',
-                'cancelButtonColor': '#d33',
-                'cancelButtonText': 'Cancelar',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire(
-                        '',
-                        'El usuario ha sido baneado',
-                        'success'
-                    )
-                }
-            })
-        });
+            botonBanear.addEventListener('click', () => {
+                Swal.fire({
+                    'icon': 'warning',
+                    'text': '¿Está seguro que desea banear al usuario?',
+                    'showCancelButton': true,
+                    'confirmButtonText': 'Sí, estoy seguro',
+                    'cancelButtonColor': '#d33',
+                    'cancelButtonText': 'Cancelar',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.fire(
+                            '',
+                            'El usuario ha sido baneado',
+                            'success'
+                        )
+                    }
+                })
+            });
 
-        let botonDescartar = document.createElement('button');
-        botonDescartar.innerText = 'Descartar';
+            let botonDescartar = document.createElement('button');
+            botonDescartar.innerText = 'Descartar';
 
-        botonDescartar.addEventListener('click', () => {
-            Swal.fire({
-                'icon': 'warning',
-                'text': '¿Está seguro que desea descartar la denuncia?',
-                'showCancelButton': true,
-                'confirmButtonText': 'Sí, estoy seguro',
-                'cancelButtonColor': '#d33',
-                'cancelButtonText': 'Cancelar',
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    Swal.fire(
-                        '',
-                        'La denuncia ha sido descartada',
-                        'success'
-                    )
-                }
-            })
-        });
-        celdaAcciones.appendChild(botonDescartar);
-        celdaAcciones.appendChild(botonBanear);
-    }});
+            botonDescartar.addEventListener('click', () => {
+                Swal.fire({
+                    'icon': 'warning',
+                    'text': '¿Está seguro que desea descartar la denuncia?',
+                    'showCancelButton': true,
+                    'confirmButtonText': 'Sí, estoy seguro',
+                    'cancelButtonColor': '#d33',
+                    'cancelButtonText': 'Cancelar',
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.fire(
+                            '',
+                            'La denuncia ha sido descartada',
+                            'success'
+                        )
+                    }
+                })
+            });
+            celdaAcciones.appendChild(botonDescartar);
+            celdaAcciones.appendChild(botonBanear);
+        }
+    });
 };
 MOSTRARTABLADENUNCIAS();
 FILTROUSUARIOS.addEventListener('keyup', MOSTRARTABLADENUNCIAS);
